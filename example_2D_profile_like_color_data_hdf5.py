ad881d75a7351cfd4cd13d9ee0944bbb88068459
Anders Kvellestad <anders.kvellestad@fys.uio.no>
2025-06-12 11:21:14 +0200

    Added feature to allow 2D profile likelihood plots to be coloured according to a chosen dataset. Also added more robust interpolation for 2D profile likelihood plots. To be extended to other plot modes.

--- example_2D_profile_like_color_data_hdf5.py
+++ example_2D_profile_like_color_data_hdf5.py
@@ -1,5 +1,6 @@
 from copy import deepcopy
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 
 import gambit_plotting_tools.gambit_plot_utils as plot_utils
@@ -24,6 +25,9 @@ datasets = [
 # Now create our main data dictionary by reading the hdf5 files
 data = plot_utils.read_hdf5_datasets([(hdf5_file, group_name)], datasets, filter_invalid_points=True)
 
+# Let's create a new dataset to demonstrate the color fill option
+data["color_data"] = 0.0 - np.sqrt(np.abs(data["sigma"] - np.median(data["sigma"])))
+
 
 # 
 # Make a 2D profile likelihood plot
@@ -36,7 +40,8 @@ likelihood_ratio_contour_values = plot_utils.get_2D_likelihood_ratio_levels(conf
 # Plot variables
 x_key = "mu"
 y_key = "sigma"
-z_key = "LogLike"
+z_key = "LogLike"     # This will be used for profiling and for contours
+c_key = "color_data"  # This will be used for the color map
 
 # Set some bounds manually?
 dataset_bounds = {
@@ -57,7 +62,8 @@ xy_bins = (100, 100)
 
 # Load default plot settings (and make adjustments if necessary)
 plot_settings = deepcopy(gambit_plot_settings.plot_settings)
-plot_settings["interpolation"] = False
+plot_settings["colormap"] = matplotlib.colormaps["plasma"]
+plot_settings["interpolation"] = True
 plot_settings["interpolation_resolution"] = 400
 
 # If variable bounds are not specified in dataset_bounds, use the full range from the data
@@ -86,7 +92,11 @@ fig, ax, cbar_ax = plot_utils.plot_2D_profile(
     contour_levels=likelihood_ratio_contour_values,
     contour_coordinates_output_file=f"./plots/2D_profile__{x_key}__{y_key}__{z_key}__coordinates.csv",
     add_max_likelihood_marker = True,
-    missing_value_color=plot_settings["colormap"](0.0),
+    color_data=data[c_key],
+    color_label=plot_labels.get(c_key, c_key),
+    color_bounds=[np.min(data[c_key]), np.max(data[c_key])],
+    color_z_condition=lambda z: z > likelihood_ratio_contour_values[-1],
+    missing_value_color=plot_settings["facecolor_plot"],
     plot_settings=plot_settings,
 )
 
@@ -107,11 +117,11 @@ fig.text(0.53, 0.79, "A very important line!", ha="left", va="center", rotation=
 
 # Draw a contour using coordinates stored in a .csv file
 x_contour, y_contour = np.loadtxt("./example_data/contour_coordinates.csv", delimiter=",", usecols=(0, 1), unpack=True)
-ax.plot(x_contour, y_contour, color="orange", linestyle="dashed", linewidth=plot_settings["contour_linewidth"], alpha=0.7)
-fig.text(0.23, 0.23, "Overlaid contour from\n coordinates in some data file", ha="left", va="center", fontsize=plot_settings["fontsize"]-5, color="orange")
+ax.plot(x_contour, y_contour, color="midnightblue", linestyle="dashed", linewidth=plot_settings["contour_linewidth"], alpha=1.0)
+fig.text(0.23, 0.23, "Overlaid contour from\n coordinates in some data file", ha="left", va="center", fontsize=plot_settings["fontsize"]-5, color="midnightblue")
 
 # Save to file
-output_path = f"./plots/2D_profile__{x_key}__{y_key}__{z_key}.pdf"
+output_path = f"./plots/2D_profile__{x_key}__{y_key}__{z_key}__{c_key}.pdf"
 plot_utils.create_folders_if_not_exist(output_path)
 plt.savefig(output_path)
 plt.close()
